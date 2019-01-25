c087e7580af050dc74e7198fead08024ba3aa91d
Willsgao <43256529+Willsgao@users.noreply.github.com>
2019-01-25 10:11:27 +0800

    Update 链表.java

--- 链表.java
+++ 链表.java
@@ -66,3 +66,54 @@ class Solution {
 		return true;
 	}
 }
+
+
+// 哨兵节点，C语言代码
+// 在数组a中，查找key，返回key所在的位置
+// n表示数组a的长度
+int find(char* a, int n, char key) {
+	// 边界条件处理，如果a为空，或者n <= 0,说明数组中没有数据，
+	// 就不用while循环比较
+	if (a == null || n <= 0) {
+		return -1;
+	}
+
+	int i = 0;
+	// 两个比较条件：i <n 和 a[i] == key
+	while (i < n) {
+		if (a[i] == key) {
+			return i;
+		}
+		++i;
+	}
+	return -1;
+}
+
+
+int find(char* a, int n, char key) {
+	if (a == null || n <= 0) {
+		return -1;
+	}
+	if (a[n-1] == key) {
+		return n-1;
+	}
+	// 把n-1的值临时保存在变量tmp中
+	// 目的是：希望find()代码不要改变a数组中的内容，
+	char tmp = a[n-1];
+	// 把key值放入a[n-1]中
+	a[n-1] = key;
+
+	int i = 0;
+	// while循环不需要进行i<n判断
+	while (a[i] != key) {
+		++i;
+	}
+	// 恢复a[n-1]原来的值
+	a[n-1] = tmp;
+
+	if (i == n-1) {
+		return -1;
+	} else {
+		return i;
+	}
+}
